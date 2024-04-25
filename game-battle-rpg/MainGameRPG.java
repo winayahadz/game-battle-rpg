cff82725af7888abf0b1b7d7c968bf031e2abde0
winayahadz <161803999+winayahadz@users.noreply.github.com>
2024-04-25 08:09:39 +0700

    Update MainGameRPG.java

--- game-battle-rpg/MainGameRPG.java
+++ game-battle-rpg/MainGameRPG.java
@@ -1,12 +1,11 @@
 public class MainGameRPG {
 //Winaya Siti Hadz Zahra	235150601111020
 //Anggy Sasmita Ningrum	235150601111021
-    
     public static void main(String[] args) {
 
         Weapon wp = new Weapon(25);
-        Hero hero = new Hero("hero1", 1, 100, 100, "Knight",wp);
-        Foe foe = new Foe(100, 100, 100, "orc");
+        Hero hero = new Hero("hero1", 1, 100, 50, "knight", wp, 50);
+        Foe foe = new Foe(1, 100, 50, "orc", wp);
 
         Potion potion = new Potion(50, 50);
         Elixir elixir = new Elixir(50, 50);
